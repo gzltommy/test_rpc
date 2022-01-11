8ad3d8edb8e2d0b220780f41d0e0acb1b1dccee3
gouzuoluo <1046292959@qq.com>
2022-01-11 16:53:17 +0800

    update

--- 02.bidstream_grpc/server/main.go
+++ 02.bidstream_grpc/server/main.go
@@ -9,20 +9,20 @@ import (
 	"strconv"
 )
 
-// Streamer 服务端
-type Streamer struct{}
+// ChatServer 服务端
+type ChatServer struct{}
 
 // BidStream 实现了 ChatServer 接口中定义的 BidStream 方法
-func (s *Streamer) BidStream(stream proto.Chat_BidStreamServer) error {
+func (s *ChatServer) BidStream(stream proto.Chat_BidStreamServer) error {
 	ctx := stream.Context()
 	for {
 		select {
 		case <-ctx.Done():
-			log.Println("收到客户端通过context发出的终止信号")
+			log.Println("收到客户端通过 context 发出的终止信号")
 			return ctx.Err()
 		default:
 			// 接收从客户端发来的消息
-			输入, err := stream.Recv()
+			request, err := stream.Recv()
 			if err == io.EOF {
 				log.Println("客户端发送的数据流结束")
 				return nil
@@ -33,7 +33,7 @@ func (s *Streamer) BidStream(stream proto.Chat_BidStreamServer) error {
 			}
 
 			// 如果接收正常，则根据接收到的 字符串 执行相应的指令
-			switch 输入.Input {
+			switch request.Input {
 			case "结束对话\n":
 				log.Println("收到'结束对话'指令")
 				if err := stream.Send(&proto.Response{Output: "收到结束指令"}); err != nil {
@@ -53,8 +53,8 @@ func (s *Streamer) BidStream(stream proto.Chat_BidStreamServer) error {
 
 			default:
 				// 缺省情况下， 返回 '服务端返回: ' + 输入信息
-				log.Printf("[收到消息]: %s", 输入.Input)
-				if err := stream.Send(&proto.Response{Output: "服务端返回: " + 输入.Input}); err != nil {
+				log.Printf("[收到消息]: %s", request.Input)
+				if err := stream.Send(&proto.Response{Output: "服务端返回: " + request.Input}); err != nil {
 					return err
 				}
 			}
@@ -67,7 +67,7 @@ func main() {
 	server := grpc.NewServer()
 
 	// 注册 ChatServer
-	proto.RegisterChatServer(server, &Streamer{})
+	proto.RegisterChatServer(server, &ChatServer{})
 
 	address, err := net.Listen("tcp", ":3000")
 	if err != nil {
